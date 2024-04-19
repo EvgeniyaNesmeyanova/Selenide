f76f759273450f64bf5ce09d38a48402e17c44df
EvgeniyaNesmeyanova <prof_nea@mail.ru>
2024-04-19 17:18:26 +0500

    fix the code according to the task condition

--- src/test/java/ru/netology/selenide/selenideTest.java
+++ src/test/java/ru/netology/selenide/selenideTest.java
@@ -8,16 +8,32 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.cssSelector;
+
 public class selenideTest {
 
     private String generateDate(long addDays, String pattern){
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
-
     @Test
     public void shouldCompleted() {
-
         open("http://localhost:9999");
-        $(cssSelector("[data-test-id='sity'] input")).setValue("Рязань");
+        $("[data-test-id='city'] input").setValue("Оренбург");
+        String planningDate=generateDate(5,"dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Петров Петр Петрович");
+        $("[data-test-id='phone'] input").setValue("+71112223355");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
+
     }
-}
+
+
+    }
+
